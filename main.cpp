1e46260b51d928ca9fd7a6fe18c0a092fda4e175
amogus <91487506+Czuowuek-SOS@users.noreply.github.com>
2022-05-03 09:58:47 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,24 +6,23 @@
 #include <ctype.h>
 
 #if defined _WIN32
-#include <windows.h>
-#include <conio.h>
+    #include <windows.h>
+    #include <conio.h>
 #elif defined (__LINUX__) || defined(__gnu_linux__) || defined(__linux__) || defined(__APPLE__)
-#include <unistd.h>
-#include <termios.h>
-char getch()
-{
-    struct termios oldt, newt;
-    char ch;
-    tcgetattr( STDIN_FILENO, &oldt );
-    newt = oldt;
-    newt.c_lflag &= ~( ICANON | ECHO );
-    tcsetattr( STDIN_FILENO, TCSANOW, &newt );
-    ch = getchar();
-    tcsetattr( STDIN_FILENO, TCSANOW, &oldt );
-    return ch;
-}
-char getch();
+    #include <unistd.h>
+    #include <termios.h>
+    char getch()
+    {
+        struct termios oldt, newt;
+        char ch;
+        tcgetattr( STDIN_FILENO, &oldt );
+        newt = oldt;
+        newt.c_lflag &= ~( ICANON | ECHO );
+        tcsetattr( STDIN_FILENO, TCSANOW, &newt );
+        ch = getchar();
+        tcsetattr( STDIN_FILENO, TCSANOW, &oldt );
+        return ch;
+    }
 #endif
 
 #define newl    '\n'
@@ -44,19 +43,23 @@ char getch();
 //#define line_Sep "\033[32m ~ \033[0m"
 #define line_Sep " ~ "
 
-
 using std::string;
 
+void screen_refresh(void);
+void clear(void);
 
+
+
+extern char input[512];
 bool program_started = false;
 int main(int argc, char *argv[2])
 {
     atexit([]()
     {   
-        if(program_started) {cls();}
+        if(program_started) {clear();}
     });
 
-    if (argc != 1)
+    if (argc < 1)
     {
         std::cout << red << "Expected arguments" << reset << newl;
     }
@@ -68,18 +71,18 @@ int main(int argc, char *argv[2])
         exit(1);
     }
 
+    clear();
     program_started = true;
-
     int input_count = 0;
     int line_count = 1;
     char c;
-    char input[512];
     while (true)
     {
         c = getch();
 
         switch (c)
         {
+            // macros
             case CTRL('q'):
             {
                 exit(0);
@@ -91,10 +94,10 @@ int main(int argc, char *argv[2])
                 fprintf(fp, "%s", input);
                 break;
             }
-
+            // special characters
             case '\n':
             {
-                std::cout << newl << line_Sep;
+                std::cout << "~" << '\r' << newl << newl;
                 line_count++;
                 break;
             }
@@ -102,14 +105,19 @@ int main(int argc, char *argv[2])
             {
                 if (input_count > 0)
                 {
-                    input[--input_count] = '\0';
+                    input_count--;
+                    input[input_count] = '\0';
                     std::cout << "\b \b";
                 }
+                break;
             }
-
-
+            // normal characters
             default:
             {
+                if (iscntrl(c) != 0)
+                {
+                    break;
+                }
 
                 input[input_count] = c;
                 input_count++;
@@ -123,10 +131,29 @@ int main(int argc, char *argv[2])
     return 0;
 }
 
+void screen_refresh(void)
+{
+    clear();
+
+    for(int i = 0 ; i < sizeof(input) / sizeof(input[0]) ; i++)
+    {
+        if(input[i] == '\0')
+        {
+            break;
+        }
+        else if(input[i] == '\n')
+        {
+            std::cout << line_Sep << newl;
+        }
+        else
+        {
+            std::cout << input[i];
+        }
+    }
+}
 
 
-
-void cls()
+void clear(void)
 {
     std::cout << "\033[2J\033[1;1H";
 }
